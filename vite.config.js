6710ffd7d8b49f8c5802cab4117edf72052f4600
imposzible9 <imposzible9@gmail.com>
2026-01-05 04:33:17 +0700

    edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { port: 8083 },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
   plugins: [react()],
 })
